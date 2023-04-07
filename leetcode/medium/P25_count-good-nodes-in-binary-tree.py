0f72e8f914dff9320b97e58e4f5dbc48c1c3e469
Trevor Ta <trevortta@gmail.com>
2023-04-06 22:49:58 -0700

    Add minimum path

---
+++ leetcode/medium/P25_count-good-nodes-in-binary-tree.py
@@ -0,0 +1,98 @@
+### PROBLEM STATEMENT
+# https://leetcode.com/problems/count-good-nodes-in-binary-tree
+
+# Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+# Return the number of good nodes in the binary tree.
+
+# Example 1:
+# Input: root = [3,1,4,3,null,1,5]
+# Output: 4
+# Explanation: Nodes in blue are good.
+# Root Node (3) is always a good node.
+# Node 4 -> (3,4) is the maximum value in the path starting from the root.
+# Node 5 -> (3,4,5) is the maximum value in the path
+# Node 3 -> (3,1,3) is the maximum value in the path.
+
+# Example 2:
+# Input: root = [3,3,null,4,2]
+# Output: 3
+# Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+# Example 3:
+# Input: root = [1]
+# Output: 1
+# Explanation: Root is considered as good.
+
+
+# Method 1:
+# 
+# Idea:
+# 
+#
+# Runtime: O(???)
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.count = 0
+        self.helper(root, root.val)
+        return self.count
+    
+    def helper(self, node, max_so_far):
+        if not node:
+            return
+        if node.val >= max_so_far:
+            self.count += 1
+        max_so_far = max(node.val, max_so_far)
+        self.helper(node.left, max_so_far)
+        self.helper(node.right, max_so_far)        
+
+### TEST UTILITIES
+
+### MAIN FUNCTIONS
+
+import unittest
+from dataclasses import dataclass
+
+class UnitTest(unittest.TestCase):
+
+    def test_1(self):
+        @dataclass
+        class TestCase:
+            name: str
+            nums: list[int]
+            expected: int
+
+        testcases = [
+            TestCase(
+                name="1",
+                nums = ,
+                expected = 
+            ),
+        ]
+
+        s = Solution()
+        for tc in testcases:
+            got = s.X(tc.nums, tc.targets)
+            self.assertEqual(
+                tc.expected,
+                got,
+                "Test {} failed. Expected {}, actual {}".format(
+                    tc.name, tc.expected, got
+                ),
+            )
+
+def main():
+    s = Solution()
+    print(s)
+
+if __name__ == '__main__':
+    main()
+    unittest.main()
---
+++ leetcode/medium/TODO_P26_path-with-minimum-effort.py
@@ -0,0 +1,106 @@
+### PROBLEM STATEMENT
+# https://leetcode.com/problems/path-with-minimum-effort/description/
+
+# You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows x columns, where heights[row][col] represents the height of cell (row, col). You are situated in the top-left cell, (0, 0), and you hope to travel to the bottom-right cell, (rows-1, columns-1) (i.e., 0-indexed). You can move up, down, left, or right, and you wish to find a route that requires the minimum effort.
+
+# A route's effort is the maximum absolute difference in heights between two consecutive cells of the route.
+
+# Return the minimum effort required to travel from the top-left cell to the bottom-right cell.
+
+# Example 1:
+# Input: heights = [[1,2,2],[3,8,2],[5,3,5]]
+# Output: 2
+# Explanation: The route of [1,3,5,3,5] has a maximum absolute difference of 2 in consecutive cells.
+# This is better than the route of [1,2,2,2,5], where the maximum absolute difference is 3.
+
+# Example 2:
+# Input: heights = [[1,2,3],[3,8,4],[5,3,5]]
+# Output: 1
+# Explanation: The route of [1,2,3,4,5] has a maximum absolute difference of 1 in consecutive cells, which is better than route [1,3,5,3,5].
+
+# Example 3:
+# Input: heights = [[1,2,1,1,1],[1,2,1,2,1],[1,2,1,2,1],[1,2,1,2,1],[1,1,1,2,1]]
+# Output: 0
+# Explanation: This route does not require any effort.
+
+# Method 1:
+# 
+# Idea: Didn't work
+# Traverse the list but didn't use Memoization
+#
+# Runtime: O(???)
+# 
+#
+class Solution1:
+    def minimumEffortPath(self, heights: list[list[int]]) -> int:
+        self.R, self.C = len(heights), len(heights[0])
+        self.H = heights
+        self.global_max_gap = 1e7
+        # self.count_paths = 0
+        self.helper(0, 0, 0, set())
+        # print('paths:', self.count_paths)
+        return self.global_max_gap
+    
+    def helper(self, row, col, max_gap, visited):
+        if max_gap > self.global_max_gap:
+            return
+        # TODO: Memoization
+        if row == self.R-1 and col == self.C-1:
+            self.global_max_gap = min(max_gap, self.global_max_gap)
+            # self.count_paths += 1
+            return
+        dirs = [(0,1), (0, -1), (1,0), (-1,0)]
+        visited.add((row, col))
+        for d in dirs:
+            new_row = row + d[0]
+            new_col = col + d[1]
+            if (new_row, new_col) in visited:
+                continue
+            if 0 <= new_row < self.R and 0 <= new_col < self.C:
+                new_val = self.H[new_row][new_col]
+                new_gap = abs(self.H[row][col] - new_val)
+                self.helper(new_row, new_col, max(max_gap, new_gap), visited)
+        visited.remove((row, col))
+
+### TEST UTILITIES
+
+### MAIN FUNCTIONS
+
+import unittest
+from dataclasses import dataclass
+
+class UnitTest(unittest.TestCase):
+
+    def test_1(self):
+        @dataclass
+        class TestCase:
+            name: str
+            nums: list[int]
+            expected: int
+
+        testcases = [
+            TestCase(
+                name="1",
+                nums = ,
+                expected = 
+            ),
+        ]
+
+        s = Solution()
+        for tc in testcases:
+            got = s.X(tc.nums, tc.targets)
+            self.assertEqual(
+                tc.expected,
+                got,
+                "Test {} failed. Expected {}, actual {}".format(
+                    tc.name, tc.expected, got
+                ),
+            )
+
+def main():
+    s = Solution()
+    print(s)
+
+if __name__ == '__main__':
+    main()
+    unittest.main()
